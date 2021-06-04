b99a25816c1f8a83ef5f0968f535be64350acd33
Aurelien <aurelien.strimelle@bstorm.be>
2021-06-04 01:36:44 -0700

    Initial commit

---
+++ app/src/main/java/be/bxl/formation/demo_05_fragement/MainActivity.java
@@ -0,0 +1,20 @@
+package be.bxl.formation.demo_05_fragement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
